4e406265f035a714226d30359a6c70e4240c8b6a
mariepastora <mpp2134@columbia.edu>
2018-05-05 17:32:13 -0400

    Scraping added

--- users.js
+++ users.js
@@ -52,25 +52,57 @@ var getJSON = function(url, callback) {
     xhr.send();
 };
 
-getJSON('https://xtools.wmflabs.org/api/page/prose/en.wikipedia.org/Albert_Einstein',
+getJSON('https://api.wikiwho.net/en/api/v1.0.0-beta/rev_content/Dog/?o_rev_id=true&editor=true&token_id=true&out=true&in=true',
 function(err, data) {
   if (err !== null) {
     console.log(err);
   } else {
-    console.log(data);
+    console.log(data.revisions[0]);
   }
 });
 
 
-// GETTING INFO THROUGH HTTP REQUEST
+// GETTING INFO THROUGH HTTP REQUEST AND SCRAPING
+
+var doc;
 
 var xhttp = new XMLHttpRequest();
 xhttp.onreadystatechange = function() {
     if (this.readyState == 4 && this.status == 200) {
        // Typical action to be performed when the document is ready:
+       doc = xhttp.responseText
+       console.log(doc)
+       doc
        document.getElementById("1var").innerHTML = xhttp.responseText;
     }
 };
 xhttp.open("GET", "https://xtools.wmflabs.org/articleinfo/en.wikipedia.org/White_supremacy", true);
 xhttp.send();
+console.log(doc)
+
+
+var xhr = new XMLHttpRequest();
+xhr.onload = function() {
+  table = this.responseXML.getElementsByClassName("top-editors-table")[0]
+  for (i=0, this.responseXML.getElementsByClassName("top-editors-table")[0].getElementsByClassName("sort-entry--username"); i< 20; i++){
+    console.log(this.responseXML.getElementsByClassName("top-editors-table")[0].getElementsByClassName("sort-entry--username")[i])
+  }
+  print(table)
+}
+xhr.open("GET", "https://xtools.wmflabs.org/articleinfo/en.wikipedia.org/White_supremacy");
+xhr.responseType = "document";
+xhr.send();
+
+function HTMLinXHR() {
+  if (!window.XMLHttpRequest)
+    return false;
+  var req = new window.XMLHttpRequest();
+  req.open('GET', window.location.href, false);
+  try {
+    req.responseType = 'document';
+  } catch(e) {
+    return true;
+  }
+  return false;
+}
 
